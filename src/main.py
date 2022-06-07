5cc280a5187e38da9e081d4859dc99789df2ca6c
to1ex <50412868+SimplyTolex@users.noreply.github.com>
2022-06-07 19:19:39 +0700

    refactor var names into snake_case

--- src/main.py
+++ src/main.py
@@ -19,119 +19,128 @@ from tkinter import *
 import random
 from tkinter import filedialog
 
-buttonList = []
-questionsList = []
-rightAnswer = ""
-playerScore = 0
+button_list = []
+questions_list = []
+right_answer = ""
+player_score = 0
 # FileValid = False     TODO: Disable button if file is invalid
 
 
 class Question():
-    def __init__(self, q, a):
-        self.quest = q
-        self.ans = a
+    def __init__(self, self_quest, self_ans):
+        self.quest = self_quest
+        self.ans = self_ans
 
 
-obj1 = Question("кто такой форд", ["фокус", "это они а не я", "твое имя", "я"])
-questionsList.append(obj1)
+default_question_1 = Question("кто такой форд", ["фокус", "это они а не я", "твое имя", "я"])
+questions_list.append(default_question_1)
 
-obj2 = Question("на чём написан РОСХУТ?", ["питон", "кобол"])
-questionsList.append(obj2)
+default_question_2 = Question("на чём написан РОСХУТ?", ["питон", "кобол"])
+questions_list.append(default_question_2)
 
 
 def button_creator(count):
-    global errorLabel
+    global error_label
     if count % 2 == 0:
         for i in range(count):
-            currentButton = Button()
+            current_button = Button()
             # If button's number is odd, then we add 0 (0.5 * 0 = 0);
-            # If, however, we add an even numbered button, then we will add 0,5 and the button will be shifted
-            currentButton.place(rely=0.5 + ((0.5 / (count / 2)) * (i // 2)),
+            # If, however, we add an even numbered button,
+            # then we will add 0,5 and the button will be shifted
+            current_button.place(rely=0.5 + ((0.5 / (count / 2)) * (i // 2)),
                                 relx=0.05 + 0.5 * (i % 2),
                                 relwidth=0.4,
                                 relheight=0.5 / (count / 2) - 0.05)
-            buttonList.append(currentButton)
-    else:
-        errorLabel.config(text="Unable to create odd number of buttons!")
+            button_list.append(current_button)
+    else:       # TODO: make odd number of answers look pretty
+        error_label.config(text="Unable to create odd number of buttons!")
 
 
-def question_render(questionNumber, correct):
-    global playerScore
-    if correct:
-        playerScore += 1
-    if questionNumber < len(questionsList):
-        global rightAnswer
-        for i in buttonList:
-            i.destroy()
-        rightAnswer = questionsList[questionNumber].ans[0]
-        buttonList.clear()
-        answerCounter = len(questionsList[questionNumber].ans)
-        button_creator(answerCounter)
-        countFreeAnswer = answerCounter
-        for i in range(answerCounter):
-            answerText = questionsList[questionNumber].ans
-            buttonList[i].config(text=answerText[random.randint(0, countFreeAnswer - 1)])
-            questionsList[questionNumber].ans.remove(buttonList[i]['text'])
-            countFreeAnswer -= 1
-            # buttonList[i].config(command=lambda: question_render(questionNumber+1))
-            if buttonList[i]["text"] == rightAnswer:
-                buttonList[i].config(command=lambda: question_render(questionNumber + 1, True))
+def question_render(current_question, is_answer_correct):
+
+    global player_score
+
+    if is_answer_correct:
+        player_score += 1
+
+    if current_question < len(questions_list):      # checks if we still have question to show
+
+        global right_answer
+
+        for i in button_list:
+            i.destroy()       # destroy buttons from previous questions to stop them from piling up
+
+        right_answer = questions_list[current_question].ans[0]
+        button_list.clear()
+        answer_count = len(questions_list[current_question].ans)        # get how many answers a question has...
+        button_creator(answer_count)                                    # ... to create just enough buttons for them
+
+        buttons_to_fill_left = answer_count
+        for i in range(answer_count):
+            answer_text = questions_list[current_question].ans
+            button_list[i].config(text=answer_text[random.randint(0, buttons_to_fill_left - 1)])
+            questions_list[current_question].ans.remove(button_list[i]['text'])
+            buttons_to_fill_left -= 1
+            if button_list[i]["text"] == right_answer:      # check if the i button happend to have a right answer
+                button_list[i].config(command=lambda: question_render(current_question + 1, True))
             else:
-                buttonList[i].config(command=lambda: question_render(questionNumber + 1, False))
-        questionLabel.config(text=questionsList[questionNumber].quest)
-    else:
-        for i in buttonList:
+                button_list[i].config(command=lambda: question_render(current_question + 1, False))
+        question_label.config(text=questions_list[current_question].quest)      # set the question
+    else:       # if we ran out of questions, destroy everyting and show the so called 'end screen'
+        for i in button_list:
             i.destroy()
-        questionLabel.config(text="Поздравляем!\nПравильных ответов:\n" + str(playerScore) + " / " + str(len(questionsList)))
+        question_label.config(text="Поздравляем!\nПравильных ответов:\n" + str(player_score) + " / " + str(len(questions_list)))        # TODO: make it fString instead to reduce clutter
 
 
-def startGame():
-    chooseFile.destroy()
-    chosenFileLabel.destroy()
-    startButton.destroy()
-    question_render(0, False)
+def start_game():
+    choose_file_button.destroy()
+    chosen_file_label.destroy()
+    start_button.destroy()
+    question_render(0, False)   # send False to question_render so it won't trigger anything
 
 
-def openFile():
-    global errorLabel
+def file_manager():
+    global error_label
     f = open(filedialog.askopenfilename(), "r", encoding="utf-8")
-    print(f)
-    questionsList.clear()
-    numberQuestion = -1
-    questionFlag = False
-    for l in f.readlines():
-        if l[0] == "?":
-            questionFlag = True
-            numberQuestion += 1
-            obj = Question(l[1:len(l)], [])
-            questionsList.append(obj)
-        elif not questionFlag:
-            errorLabel.config(text="QuestionsFile Syntax Error: there is no question to add answer to!")
-            print("hi")
-        elif l[0] == "#":
+    # print(f)      # sends filepath to debug console
+    questions_list.clear()
+    question_number = -1
+    question_flag = False
+
+    for currentLine in f.readlines():
+        if currentLine[0] == "?":   # if this line is a question
+            question_flag = True     # set the flag so it will not trigger on the next line (which should be the answer)
+            question_number += 1
+            question_to_append = Question(currentLine[1:len(currentLine)], [])
+            questions_list.append(question_to_append)
+        elif not question_flag:
+            error_label.config(text="QuestionsFile Syntax Error: there is no question to add answer to!")
+        elif currentLine[0] == "#":     # if this line is comment
             pass    # TODO: add comment functionality
-        else:
-            questionsList[numberQuestion].ans.append(l)
-    chosenFileLabel.config(text="Выбран файл: " + str(f)[25:len(str(f))-28])
+        else:       # if this line is answer
+            questions_list[question_number].ans.append(currentLine)
+    chosen_file_label.config(text="Выбран файл: " + str(f)[25:len(str(f))-28])
     f.close()
 
 
 root = Tk()
 root.geometry("640x480")
-root.title("ROSHOOT? CLIENT v0.3")
+root.title("ROSHOOT? CLIENT v0.3")      # TODO: move version number into conf file (that doesn't exist yet)
 
-questionLabel = Label(root, font=("Arial", "30"))
-questionLabel.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.4)
+error_label = Label(text="")
+error_label.pack()
 
-chosenFile = "Выбраны стандартные вопросы (выберите свои с помощью кнопки сверху)"
-errorLabel = Label(text="")
-errorLabel.pack()
+question_label = Label(root, font=("Arial", "30"))
+question_label.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.4)
 
-chooseFile = Button(root, text="Открыть файл с вопросами", command=openFile)
-chooseFile.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.6)
-chosenFileLabel = Label(root, text=chosenFile)
-chosenFileLabel.place(relwidth=1, relheight=0.1, relx=0, rely=0.7)
-startButton = Button(root, text="Начать викторину!", command=startGame)
-startButton.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.8)
+chosen_file = "Выбраны стандартные вопросы (выберите свои с помощью кнопки сверху)"
+choose_file_button = Button(root, text="Открыть файл с вопросами", command=file_manager)
+choose_file_button.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.6)
+
+chosen_file_label = Label(root, text=chosen_file)
+chosen_file_label.place(relwidth=1, relheight=0.1, relx=0, rely=0.7)
+
+start_button = Button(root, text="Начать викторину!", command=start_game)
+start_button.place(relwidth=0.5, relheight=0.1, relx=0.25, rely=0.8)
 
 root.mainloop()
